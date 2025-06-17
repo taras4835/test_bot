e0edeb68f97b5689729e8f78db7a4670c3e710e3
taras4835 <112586133+taras4835@users.noreply.github.com>
2025-06-17 15:49:19 +0300

    fix

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import telebot
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
 
-API_TOKEN = '7836172114:AAH1qNbMF9EE8ROmazgYeAt4ST-kojT16CU'
+load_dotenv()
+
+API_TOKEN = os.getenv('API_TOKEN')
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
 
 bot = telebot.TeleBot(API_TOKEN)
 
@@ -17,7 +24,17 @@ I am here to echo your kind words back to you. Just say anything nice and I'll s
 # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
 @bot.message_handler(func=lambda message: True)
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    
+    
+    client = OpenAI(api_key=OPENAI_API_KEY)
+
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=message.text
+    )
+
+    bot.reply_to(message, response.output_text)
+
 
 
 bot.infinity_polling()
